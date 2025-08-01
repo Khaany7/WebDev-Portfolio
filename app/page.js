1b97ac01db58b1860658055f88b2001b8e5290b5
khaany7 <abdurrehmankhan4946@gmail.com>
2025-08-01 16:20:56 +0500

    commented uncommented

--- app/page.js
+++ app/page.js
@@ -19,7 +19,7 @@ export default async function Home() {
       {/* <Projects /> */}
       {/* <Education /> */}
       {/* <Blog blogs={blogs} /> */}
-      {/* <ContactSection /> */}
+      <ContactSection />
     </div>
   )
 };
