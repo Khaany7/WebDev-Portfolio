11f8ec3549fcdbbca833e8f3f2f3947f1bebf4a2
khaany7 <abdurrehmankhan4946@gmail.com>
2025-08-01 16:26:00 +0500

    exp com

--- app/page.js
+++ app/page.js
@@ -14,7 +14,7 @@ export default async function Home() {
     <div suppressHydrationWarning >
       <HeroSection />
       <AboutSection />
-      <Experience />
+      {/* <Experience /> */}
       {/* <Skills /> */}
       {/* <Projects /> */}
       {/* <Education /> */}
