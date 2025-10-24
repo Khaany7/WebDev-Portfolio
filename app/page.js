d114902fa06b0122e4a716b11311166e9a2a88e4
Abdurrehman Yousafzai <abdurrehmankhan4946@gmail.com>
2025-10-25 00:04:44 +0500

    ssr set in page.js

--- app/page.js
+++ app/page.js
@@ -1,11 +1,15 @@
 "use client";
 import AboutSection from "./components/homepage/about";
 import ContactSection from "./components/homepage/contact";
-import Education from "./components/homepage/education";
-import Experience from "./components/homepage/experience";
+// import Education from "./components/homepage/education";
+// import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
+
+
+const Education = dynamic(() => import("./components/homepage/education"), { ssr: false });
+const Experience = dynamic(() => import("./components/homepage/experience"), { ssr: false });
 
 
 export default function Home() {
