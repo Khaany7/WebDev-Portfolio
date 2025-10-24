b83987ec9f3e07395e00764db689bcc9e8fc9ee0
Abdurrehman Yousafzai <abdurrehmankhan4946@gmail.com>
2025-10-25 00:21:58 +0500

    Footer changes

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -16,7 +16,7 @@ function Footer() {
           <div className="flex items-center gap-5">
             <Link
               target="_blank"
-              href="https://github.com/Khaany7/developer-portfolio"
+              href="https://github.com/Khaany7"
               className="flex items-center gap-2 uppercase hover:text-[#16f2b3]"
             >
               <IoStar />
@@ -24,7 +24,7 @@ function Footer() {
             </Link>
             <Link
               target="_blank"
-              href="https://github.com/Khaany7/developer-portfolio/fork"
+              href="https://github.com/Khaany7"
               className="flex items-center gap-2 uppercase hover:text-[#16f2b3]"
             >
               <CgGitFork />
