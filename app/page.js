e9c55762b7b68aed4139536145904313d93966e3
Abdurrehman Yousafzai <abdurrehmankhan4946@gmail.com>
2025-10-25 00:06:11 +0500

    dynamic import

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 "use client";
+import dynamic from "next/dynamic";
 import AboutSection from "./components/homepage/about";
 import ContactSection from "./components/homepage/contact";
 // import Education from "./components/homepage/education";
