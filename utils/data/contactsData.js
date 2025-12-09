cb1881a89f5ae7406dc052a0ecaab4340f111f4d
Abdurrehman Yousafzai <93050852+Khaany7@users.noreply.github.com>
2025-12-09 14:05:19 +0500

    info@abdurrehmanyousafzai.dev

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,5 +1,5 @@
 export const contactsData = {
-    email: 'abdurrehmankhan4946@gmail.com',
+    email: 'info@abdurrehmanyousafzai.dev',
     phone: '+92 322 5832829',
     address: 'PECHS BLOCK 06, Karachi, Pakistan - 07545 ',
     github: 'https://github.com/Khaany7',
